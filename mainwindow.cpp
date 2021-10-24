c10167ea10629c4c80cfa3a30dc3df1fb7e746f6
rnjsrjs0417 <rnjsrjs0417@naver.com>
2021-10-24 21:49:11 +0900

    Clock
    clock hello ~

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QDateTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    timer = new QTimer(this);
+    connect(timer, SIGNAL(timeout()), this, SLOT (myfunction()));
+    timer->start(1000); // 1초마다 타이밍을 재야됨 , 현재시간일단 띠용받아와서 그떄부터 잼
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::myfunction()
+{
+    //qDebug() << "update..";
+    QTime time = QTime::currentTime();
+    QString time_text = time.toString("hh : mm : ss");
+    ui->label_date_time->setText(time_text);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QTimer>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,8 +14,11 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+public slots:
+    void myfunction();
 
 private:
     Ui::MainWindow *ui;
+    QTimer *timer;
 };
 #endif // MAINWINDOW_H
