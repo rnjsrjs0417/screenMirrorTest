37f5f16e49954c4a5e87b56b6c77d2b54650e325
rnjsrjs0417 <rnjsrjs0417@naver.com>
2021-11-23 22:56:59 +0900

    .
    .

--- secdialog.cpp
+++ secdialog.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "secdialog.hpp"
 #include "ui_secdialog.h"
 #include "mainwindow.h"
@@ -66,48 +65,3 @@ void SecDialog::loginComplete(int a)
     n->sampling();
 }
 
-=======
-#include "secdialog.hpp"
-#include "ui_secdialog.h"
-#include "mainwindow.h"
-#include <QRandomGenerator>
-#include <QMessageBox>
-
-SecDialog::SecDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::SecDialog)
-{
-    ui->setupUi(this);
-    setWindowFlags(Qt::FramelessWindowHint);
-
-
-}
-
-SecDialog::~SecDialog()
-{
-    delete ui;
-}
-
-void SecDialog::on_pushButton_clicked()
-{
-    //hide();
-    n=new MainWindow(this);
-
-    n->show();
-     n->sampling();
-}
-
-
-void SecDialog::on_pushButton_2_clicked()
-{
-    quint64 v = QRandomGenerator::global()->bounded(1000, 9999);
-   // QMessageBox::about(this,"Number",QString::number(v));
-
-      QMessageBox msg;
-      msg.setText(QString::number(v));
-      msg.setStyleSheet("QLabel{min-width: 700px;}");
-
-      msg.exec();
-}
-
->>>>>>> fbca8041db033b145428070811c624f39f4cb251
