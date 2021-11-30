0ec36e9d193d0f03b922f3a87d1af300e8c96ac9
DESKTOP-CPNPUEO\Eungu_Kwon <dmsrn135@knu.ac.kr>
2021-11-30 22:50:42 +0900

    Change NetConnection class mechanism to Qt library

--- netconnection.cpp
+++ netconnection.cpp
@@ -8,56 +8,15 @@
 #define TCP_PORT 16005
 using namespace std;
 
-char buffer[10000];
 int i = 0 ;
 
-NetConnection::NetConnection(SecDialog* w, string server, void (SecDialog::*_callback)(int)){
-    WSADATA wsaData;
-    struct hostent *host;
-    SOCKADDR_IN http_SockAddr;
-
-    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0){
-        qDebug() << "WSAStartup failed.";
-    }
-
+NetConnection::NetConnection(SecDialog* w, string server) : QObject(w){
+    manager = new QNetworkAccessManager(this);
     server_url = server;
     window = w;
-    callback = _callback;
-
-    HTTP_Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    host = gethostbyname(server_url.c_str());
-
-    http_SockAddr.sin_port=htons(HTTP_PORT);
-    http_SockAddr.sin_family=AF_INET;
-    http_SockAddr.sin_addr.s_addr = *((unsigned long*)host->h_addr);
-
-    if(connect(HTTP_Socket,(SOCKADDR*)(&http_SockAddr),sizeof(http_SockAddr)) != 0){
-        qDebug() << "Could not connect HTTP server";
-        return;
-    }
 }
 
 NetConnection::~NetConnection(){
-    closesocket(TCP_Socket);
-    closesocket(HTTP_Socket);
-    WSACleanup();
-}
-
-void NetConnection::make_socket(){
-    SOCKADDR_IN sock_sockAddr;
-    struct hostent *host;
-
-    TCP_Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    host = gethostbyname(server_url.c_str());
-
-    sock_sockAddr.sin_port=htons(TCP_PORT);
-    sock_sockAddr.sin_family=AF_INET;
-    sock_sockAddr.sin_addr.s_addr = *((unsigned long*)host->h_addr);
-
-    if(connect(TCP_Socket,(SOCKADDR*)(&sock_sockAddr),sizeof(sock_sockAddr)) != 0){
-        qDebug() << "Could not connect TCP server";
-        return;
-    }
 }
 
 void NetConnection::thread_end(){
@@ -66,140 +25,98 @@ void NetConnection::thread_end(){
     (window->*pFunc)(123);
 }
 
-int NetConnection::get_devicecode(){
+int NetConnection::get_devicecode(void (SecDialog::*_callback)(int)){
     char b[] = "device";
-    make_socket();
-    send(TCP_Socket, b, strlen(b), 0);
+    callback = _callback;
 
-    if(recv(TCP_Socket, buffer, sizeof(buffer), 0) <= 0){
-        return -1;
-    }
+    socket = new QTcpSocket(this);
+    socket->connectToHost(server_url.c_str(), 16005);
+    socket->waitForConnected();
+    socket->write("device");
+    socket->waitForReadyRead(30000);
 
-    RecvThread* rt = new RecvThread(this, &TCP_Socket);
-    rt->start();
+    QString str(socket->readAll());
 
-    return atoi(buffer);
+    connect(socket, SIGNAL(readyRead()), this, SLOT(ready2readCode()));
+
+    return atoi(str.toStdString().c_str());
 }
 
-int NetConnection::load_user(string mid){
+void NetConnection::load_user(string mid, void (SecDialog::*_callback)(int)){
     string q = "/validuser?id=" + mid;
-    map<string, string> res = http_request(q);
-    if((*res.find("_result")).second == "T")
-        if((*res.find("result")).second == "true") return 0;
-    return -1;
-}
+    callback = _callback;
 
-void NetConnection::cancel_code(){
-    closesocket(TCP_Socket);
+    string url = "http://" + server_url + q;
+    qDebug() << url.c_str();
+    QUrl serviceURL(url.c_str());
+    QNetworkRequest request(serviceURL);
+
+    reply = manager->get(request);
+    connect(reply, SIGNAL(readyRead()), this, SLOT(ready2read()));
 }
 
 int NetConnection::sendHealthData(int pulse, int max, int min, int spo2){
-    string q = "/addHealth?id=" + id + "&pulse=" + to_string(pulse) + "&max=" + to_string(max) + "&min=" + to_string(min) + "&spo2=" + to_string(spo2);
+    string q = ":16002/addHealth?id=" + id + "&pulse=" + to_string(pulse) + "&max=" + to_string(max) + "&min=" + to_string(min) + "&spo2=" + to_string(spo2);
 
-    map<string, string> res = http_request(q);
-    if((*res.find("_result")).second == "T")
-        if((*res.find("result")).second == "true") return 0;
-    return -1;
+    string url = "http://" + server_url + q;
+
+    QUrl serviceURL(url.c_str());
+    QNetworkRequest request(serviceURL);
+
+    reply = manager->get(request);
+    return 0;
 }
 
-map<string, string> NetConnection::http_request(string query){
-    string get_http, website_HTML;
-    map<string, string> ret;
-    get_http = "GET " + query + " HTTP/1.1\r\nHost: " + server_url + ":" + to_string(HTTP_PORT) + "\r\nAccept: */*\r\n\r\n";
+void NetConnection::new_load_user(string str){
+    manager = new QNetworkAccessManager(this);
 
-    send(HTTP_Socket,get_http.c_str(), strlen(get_http.c_str()),0 );
+    string url = server_url + str;
+    QUrl serviceURL(url.c_str());
+    QNetworkRequest request(serviceURL);
 
-    int nDataLength, isKey = 2, go_loop = 1, space_after_col = 0;
-    string headerkey, headerval, startline;
-    map<string, string> header;
-    while ((nDataLength = recv(HTTP_Socket,buffer,1,0)) > 0 && go_loop){
-        char c = buffer[0];
-        switch(c){
-            case '\n': break;
-            case '\r':
-                if(isKey == 2){
-                    startline = headerval;
-                    headerval.clear();
-                    isKey = 1;
-                }
-                else{
-                    if(isKey == 1){
-                        go_loop = 0;
-                    }
-                    else{
-                        header.insert({headerkey, headerval});
-                        headerkey.clear();
-                        headerval.clear();
-                        isKey = 1;
-                    }
-                }
-            break;
+    reply = manager->get(request);
 
-            default:
-                if(c >= 32){
-                    if(c == ' ' && space_after_col){
-                        space_after_col = 0;
-                        break;
-                    }
-                    if(c == ':') {
-                        if(isKey == 0) headerval += c;
-                        else{
-                            isKey = 0;
-                            space_after_col = 1;
-                        }
-                    }
-                    else if(isKey == 1){
-                        headerkey += c;
-                    }
-                    else {
-                        headerval += c;
-                    }
-                }
-            break;
-        }
-    }
-
-    int contentsize = 0, recvsize = 0;
-    if(header.find("Content-Length") != header.end()){
-        contentsize = stoi((*header.find("Content-Length")).second);
-    }
-    while (recvsize < contentsize && (nDataLength = recv(HTTP_Socket,buffer,contentsize,0)) > 0){
-        website_HTML = buffer;
-        recvsize += nDataLength;
-    }
-
-    if(header.find("Content-Type") != header.end()){
-        ret = map_from_json(website_HTML);
-        ret.insert({"_result", "T"});
-    }
-    else ret.insert({"_result", "F"});
-    return ret;
 }
 
-map<string, string> NetConnection::map_from_json(string str){
-    int str_size = str.size();
-    int is_key = 1;
-    string k, v;
-    map<string, string> ret;
-    for(int i = 1; i < str_size - 1; i++){
-        if(str[i] == ':'){
-            is_key = 1 - is_key;
-            continue;
-        }
-        if(str[i] == '"') continue;
-        if(str[i] == ','){
-            ret.insert({k, v});
-            k.clear();
-            v.clear();
-            continue;
-        }
-        if(is_key){
-            k += str[i];
-        }
-        else{
-            v += str[i];
-        }
+void NetConnection::ready2read(){
+    QJsonDocument document = QJsonDocument::fromJson(reply->readAll());
+    QJsonObject object = document.object();
+
+    auto value = object.value("result");
+    bool result = value.toBool();
+
+    if(result){
+        void(SecDialog::*pFunc)(int) = callback;
+
+        (window->*pFunc)(123);
     }
-    ret.insert({k, v});
-    return ret;
+//    QJsonValue value = object.value("agentsArray");
+//    QJsonArray array = value.toArray();
+}
+
+void NetConnection::ready2readCode(){
+    QString data_ = socket->readAll();
+    string data = data_.toStdString();
+    int is_id = 1;
+    string id_temp, name_temp;
+    if(data.size() == 0) return;
+    for(int i = 0; i < data.size(); i++){
+        if(data[i] == '\n'){
+            if(is_id == 1) {
+                is_id = 0;
+            }
+            else {
+                break;
+            }
+            continue;
+        }
+        if(is_id == 1) id_temp += data[i];
+        else name_temp += data[i];
+    }
+    id = id_temp;
+    name = name_temp;
+
+    qDebug() << id.c_str() << name.c_str();
+
+    thread_end();
 }
--- netconnection.h
+++ netconnection.h
@@ -1,27 +1,36 @@
 #ifndef NETCONNECTION_H
 #define NETCONNECTION_H
 
+#include <QObject>
 #include <QtDebug>
 #include <map>
 #include <string>
-#include <winsock2.h>
-#include <windows.h>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkReply>
+#include <QJsonDocument>
+#include <QJsonObject>
 
 class SecDialog;
 
-class NetConnection
+class NetConnection : public QObject
 {
+    Q_OBJECT
 public:
-    NetConnection(SecDialog* w, std::string server, void (SecDialog::*_callback)(int));
+    NetConnection(SecDialog* w, std::string server);
     ~NetConnection();
 
     bool init_network(std::string server);
 
-    int get_devicecode();
-    void cancel_code();
+    int get_devicecode(void (SecDialog::*_callback)(int));
     int sendHealthData(int pulse, int max, int min, int spo2);
     void thread_end();
-    int load_user(std::string mid);
+    void load_user(std::string mid, void (SecDialog::*_callback)(int));
+    void new_load_user(std::string mid);
+
+public slots:
+    void ready2read();
+    void ready2readCode();
 
 public:
     std::string id;
@@ -30,15 +39,15 @@ public:
 private:
     void make_socket();
     void recv_thread();
-    std::map<std::string, std::string> map_from_json(std::string str);
-    std::map<std::string, std::string> http_request(std::string query);
 
 private:
-    SOCKET TCP_Socket;
-    SOCKET HTTP_Socket;
     std::string server_url;
     SecDialog* window;
+    QTcpSocket* socket;
+    QNetworkReply * reply;
+    QNetworkAccessManager* manager;
     void (SecDialog::*callback)(int);
+
 };
 
 
